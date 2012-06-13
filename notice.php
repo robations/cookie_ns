6c3738e4da1b89f747026044d607b5fb14e96403
Robert Churchill <robert@idacmedia.com>
2012-06-13 14:02:51 +0100

    More defensive style rules

--- notice.php
+++ notice.php
@@ -1,6 +1,6 @@
 <?php include_once(dirname(__FILE__) . '/config.php'); ?>
 <?php if (empty($_COOKIE[$cookieNonsenseSettings['cookie_name']])): ?>
-<div class="cookieNonsense-container" id="cookieNonsense" style="background: #fff; border: 1px solid #ccc; padding: 20px; position: fixed; bottom: 20px; left: 20px; width: 16em; text-align: center;">
+<div class="cookieNonsense-container" id="cookieNonsense" style="background: #fff; border: 1px solid #ccc; color: #000; font-family: Arial, sans-serif; padding: 20px; position: fixed; bottom: 20px; left: 20px; width: 16em; text-align: center; z-index: 100;">
     <p class="cookieNonsense-header" style="font-size: 14px; font-weight: bold;"><?php echo $cookieNonsenseSettings['heading']; ?></p>
     <?php if (!empty($cookieNonsenseSettings['policy_url'])): ?>
     <p class="cookieNonsense-policy"><a class="cookieNonsense-policyLink" href="<?php echo $cookieNonsenseSettings['policy_url']; ?>">
